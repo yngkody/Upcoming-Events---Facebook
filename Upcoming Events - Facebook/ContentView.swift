0b48ef42ea4f7b446d435bf3207e3334bf81ac57
Teneala Spencer <teneala.spencer@usm.edu>
2021-08-12 19:39:07 -0400

    Initial Commit

---
+++ Upcoming Events - Facebook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Upcoming Events - Facebook
+//
+//  Created by Kody Young on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
